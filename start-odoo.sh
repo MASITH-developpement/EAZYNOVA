d64232796b1685647df2e078ccc14794dee71564
MASITH <contact@stephane-moreau.fr>
2025-11-17 21:56:55 +0100

    Script Odoo : DB_HOST universel, attente et substitution

--- start-odoo.sh
+++ start-odoo.sh
@@ -1,25 +1,26 @@
-# Vérification et log de la variable Railway
-echo "RAILWAY_PRIVATE_DOMAIN = $RAILWAY_PRIVATE_DOMAIN"
-if [ -z "$RAILWAY_PRIVATE_DOMAIN" ]; then
-	echo "ERREUR : RAILWAY_PRIVATE_DOMAIN n'est pas défini !"
-	exit 1
-fi
-# Remplacement dynamique du host dans la config Odoo
-sed -i "s|db_host = .*|db_host = ${RAILWAY_PRIVATE_DOMAIN}|" /etc/odoo/odoo.conf
+
 #!/bin/bash
 # Script de démarrage Odoo avec affichage de l'IP publique et choix du nom de base
 
+# Vérification et log de la variable DB_HOST
+echo "DB_HOST = $DB_HOST"
+if [ -z "$DB_HOST" ]; then
+  echo "ERREUR : DB_HOST n'est pas défini !"
+  exit 1
+fi
+# Remplacement dynamique du host dans la config Odoo
+sed -i "s|db_host = .*|db_host = $DB_HOST|" /etc/odoo/odoo.conf
 
 # Attendre que PostgreSQL soit prêt
 echo "Attente de PostgreSQL..."
 for i in {1..30}; do
-	nc -z eazynova.railway.internal 5432 && break
-	echo "PostgreSQL non prêt, tentative $i/30..."
-	sleep 2
+  nc -z $DB_HOST 5432 && break
+  echo "PostgreSQL non prêt, tentative $i/30..."
+  sleep 2
 done
 
 IP=$(curl -s ifconfig.me)
-echo "Accédez à Odoo sur : http://$IP:8069"
+echo "Accédez à Odoo sur : http://$IP:80"
 
 # Affiche la valeur réelle de la variable d'environnement
 echo "ODOO_DB_NAME = $ODOO_DB_NAME"
