f202f1110d8463c7e08e0971b3cffe9e03f7b821
MASITH <contact@stephane-moreau.fr>
2025-11-22 11:50:20 +0100

    fix: vues wizard IA & facial, ajout chantier

---
+++ addons/eazynova_chantier/__manifest__.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+{
+    'name': 'EAZYNOVA Chantier',
+    'version': '19.0.1.0.0',
+    'category': 'Project',
+    'summary': 'Gestion de chantiers avec IA et gestion documentaire',
+    'author': 'EAZYNOVA',
+    'website': 'https://eazynova-production.up.railway.app/',
+    'license': 'LGPL-3',
+    'depends': ['eazynova'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/chantier_sequence.xml',
+        'views/chantier_menu.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ addons/eazynova_chantier/models/__init__.py
@@ -0,0 +1,5 @@
+﻿# -*- coding: utf-8 -*-
+
+from . import chantier
+from . import chantier_task
+from . import chantier_document
